a950d266f1e9c52ea9738b7ed27f0bd61e8aa6b0
PritamDutta <webdevpritam7498@gmail.com>
2023-07-26 21:11:35 +0600

    creating query and his value

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ async function run() {
 
       // bookings--------
       app.get('/bookings', async(req, res)=>{
+        console.log(req.query);
         const result =  await bookingCollection.find().toArray();
         res.send(result);
       })
